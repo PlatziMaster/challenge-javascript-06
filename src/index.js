45872e6b21f2cc694f1c7a6f471b83aac477f035
eriksape <erik.sape@gmail.com>
2020-02-06 08:21:09 -0600

    solving challenge

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations = [] , currentCombination  = [], startFrom = 0) => {
+  if (remainingSum == 0) {
+    let temporal = currentCombination.slice();
+    finalCombinations.push(temporal);
+    return [];
+  }
+
+for (let i=startFrom; i<candidates.length; i++) {
+  if(remainingSum < candidates[i]) 
+    return [];
+    currentCombination.push(candidates[i]);
+    combinationSumRecursive(candidates, remainingSum - candidates[i], finalCombinations, currentCombination, i);
+    currentCombination.pop(); 
   }
   
+  return finalCombinations;
+}
+
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
    *
@@ -19,7 +33,7 @@ const combinationSumRecursive = (
    * @return {number[][]}
    */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+  return combinationSumRecursive(candidates, target);
+}
 
 module.exports = combinationSum;

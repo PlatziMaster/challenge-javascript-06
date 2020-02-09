9d17230c9a32cea57918b9d6a0e4886619a2cbb5
Omar Flores Grimontt <omar.flores@3dves.com>
2020-02-09 11:25:06 -0500

    Solved combination sum

--- src/index.js
+++ src/index.js
@@ -7,19 +7,35 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations, currentCombination, startFrom) => {
+  if (remainingSum < 0) {
+    return finalCombinations;
   }
-  
-  /**
-   * Backtracking algorithm of finding all possible combination for specific sum.
-   *
-   * @param {number[]} candidates
-   * @param {number} target
-   * @return {number[][]}
-   */
+
+  if (remainingSum === 0) {
+    finalCombinations.push([...currentCombination]);
+    return finalCombinations;
+  }
+
+  candidates.slice(startFrom).forEach((candidate, index) =>{
+    currentCombination.push(candidate)
+    combinationSumRecursive(candidates, remainingSum - candidate, finalCombinations, currentCombination, index + startFrom)
+    currentCombination.pop()
+  })
+
+  return finalCombinations
+}
+
+/**
+ * Backtracking algorithm of finding all possible combination for specific sum.
+ *
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+  return combinationSumRecursive(candidates, target, [], [], 0);
+}
 
 module.exports = combinationSum;

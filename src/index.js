f5df211127b7c33dd84d5290a48bb80b9ccad75c
= <jonne@LAPTOP-V6AUKDD4.localdomain>
2020-03-09 10:40:12 -0600

    solución en respuesta al reto

--- src/index.js
+++ src/index.js
@@ -7,8 +7,41 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+  function combinationSumRecursive (
+    candidates, //conjunto inicial de candidatos 
+    remainingSum, //balance de la suma
+    finalCombinations = [], 
+    currentCombination = [], //es el arreglo donde vamos a guardar nuestros datos temporalmente "seguimiento"
+    startFrom = 0, //comienza a explorar desde el primer candidato del array
+  ) {
+    //si el balance de la suma es negativo significa que el último candidato no debe ser aceptado y volvemos a las combinaciones
+    if (remainingSum < 0) {
+      return finalCombinations;
+    }
+    //si la suma coincide con nuestro número objetivo entra al arreglo final
+    if (remainingSum ===0) {
+      finalCombinations.push(currentCombination.slice());
+
+      return finalCombinations;
+    }
+    //revisamos si hay más candidatos o el combo ha sido completado
+    for (let index = startFrom; index < candidates.length; index += 1) { 
+      const currentCandidate = candidates [index];
+    //si hay más candidatos son enviados a nuestro arreglo de seguimiento
+      currentCombination.push(currentCandidate);
+      // exploramos las opciones del nuevo candidato...
+      combinationSumRecursive(
+        candidates, 
+        remainingSum - currentCandidate,
+        finalCombinations,
+        currentCombination,
+        index
+      );
+      //volver e intentar...
+      currentCombination.pop();
+    }
+    //obtenemos todas nuestras combinaciones de suma
+    return finalCombinations;
   }
   
   /**
@@ -18,8 +51,8 @@ const combinationSumRecursive = (
    * @param {number} target
    * @return {number[][]}
    */
-const combinationSum = (candidates, target) => {
+  const combinationSum = (candidates, target) => {
     return combinationSumRecursive(candidates, target);
   }
 
-module.exports = combinationSum;
+export default combinationSum;

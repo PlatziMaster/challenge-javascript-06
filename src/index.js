08a31e32071cbaf8deef42f6d1640166b9f18119
Ruben Padilla <rubbenpad2@gmail.com>
2020-04-04 23:04:05 -0500

    challenge-javascript-06 solved

--- src/index.js
+++ src/index.js
@@ -1,25 +1,36 @@
 /**
  * @param {number[]} candidates - candidate numbers we're picking from.
- * @param {number} remainingSum - remaining sum after adding candidates to currentCombination.
- * @param {number[][]} finalCombinations - resulting list of combinations.
- * @param {number[]} currentCombination - currently explored candidates.
- * @param {number} startFrom - index of the candidate to start further exploration from.
+ * @param {number} target - number we're searching possible sum combinations
+ * @param {number} index - index of the candidate to start further exploration from.
+ * @param {number[][]} combinations - resulting list of combinations.
+ * @param {number[]} current - currently explored candidates.
  * @return {number[][]}
  */
+const combinationSum = (
+  candidates,
+  target,
+  index = 0,
+  current = [],
+  combinations = []
+) => {
+  // Case base
+  if (target < 0) {
+    return;
+  }
 
-const combinationSumRecursive = (
-    
+  if (target === 0) {
+    combinations.push([...current]);
+    return;
   }
-  
-  /**
-   * Backtracking algorithm of finding all possible combination for specific sum.
-   *
-   * @param {number[]} candidates
-   * @param {number} target
-   * @return {number[][]}
-   */
-const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
+
+  for (let i = index; i < candidates.length; i++) {
+    const currentValue = candidates[i];
+    current.push(currentValue);
+    combinationSum(candidates, target - currentValue, i, current, combinations);
+    current.pop();
   }
+
+  return combinations;
+};
 
 module.exports = combinationSum;

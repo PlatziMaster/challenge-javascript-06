ff26faf8804f65930c4bb87c674cd2eb7b5b36b8
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-04-12 21:45:46 -0500

    Primera versión de la Propuesta

--- src/index.js
+++ src/index.js
@@ -7,9 +7,86 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
+const combinationSumRecursive = (candidates, target)=>{
+  
+  let grupoSolucion = []
+  let suma = 0
+  let solucion = []
+  let i = 0
+  let z = candidates.length;
+
+  for (let index = 0; index < candidates.length; index++) {
+    i = index
+    const Number = candidates[index];
+
+    if (Number == target) {
+      grupoSolucion.push([Number])
+        break
+      }
+    if((target%2 == 0) && (Number%2 == 0)){
+          while(suma < target){
+              suma += candidates[i]
+              solucion.push(candidates[i])
+              if (suma == target) {
+                grupoSolucion.push(solucion)
+                suma = 0              
+                solucion = []
+              break
+            }
+          }
+      } 
+    
+      while (suma < target && i < z) {
+          if (candidates[i] + suma > target) {
+            break
+          }else{          
+            suma += candidates[i]
+            solucion.push(candidates[i])
+            i++
+          }
+          if (suma == target) {
+            grupoSolucion.push(solucion.sort())
+            suma = 0              
+            solucion = []
+            break
+          }
+      }
+
+      if (suma == target) break
+
+      while(suma < target ){
+
+        if (z == 1) {
+          suma += candidates[i-1]
+          solucion.push(candidates[i-1])
+        }else{
+          
+          while((candidates[i] + suma > target) && i >= index){
+            i--
+          }
+          if (i >= index) {
+            suma += candidates[i]
+            solucion.push(candidates[i])
+          }            
+        }
+        if (suma == target) {
+          grupoSolucion.push(solucion.sort())
+          suma = 0              
+          solucion = []
+          break
+        }
+        if(i <= index){
+          suma = 0              
+          solucion = []
+          break
+         }  
+      }
     
   }
+  
+  return grupoSolucion
+ 
+}
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.

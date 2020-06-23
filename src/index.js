46f5e7d96c2c92f3ec682366ab82ab7d3ae51a23
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-06-23 13:46:22 -0500

    the solution was provided

--- src/index.js
+++ src/index.js
@@ -8,18 +8,46 @@
  */
 
 const combinationSumRecursive = (
-    
+  candidates,
+  remainingSum,
+  finalCombinations = [],
+  currentCombination = [],
+  startFrom = 0
+) => {
+  if (candidates.length === 0) return [];
+
+  if (remainingSum < 0) return;
+
+  if (remainingSum === 0) {
+    finalCombinations.push([...currentCombination]);
+    return;
   }
-  
-  /**
-   * Backtracking algorithm of finding all possible combination for specific sum.
-   *
-   * @param {number[]} candidates
-   * @param {number} target
-   * @return {number[][]}
-   */
+
+  for (let index = startFrom; index < candidates.length; index++) {
+
+    combinationSumRecursive(
+      candidates,
+      remainingSum - candidates[index],
+      finalCombinations,
+      currentCombination = [...currentCombination, candidates[index]],
+      startFrom + index
+    )
+    currentCombination.pop();
+  }
+
+  return finalCombinations;
+
+}
+
+/**
+ * Backtracking algorithm of finding all possible combination for specific sum.
+ *
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+  return combinationSumRecursive(candidates, target);
+}
 
 module.exports = combinationSum;

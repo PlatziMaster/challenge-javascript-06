fe09cc30994f84b5bcfdfa8622712d117c06c490
jose caicedo <joseluiscl@gmail.com>
2020-08-20 15:50:59 -0500

    Backtracking algorithm of finding all possible combination for specific sum

--- src/index.js
+++ src/index.js
@@ -6,10 +6,41 @@
  * @param {number} startFrom - index of the candidate to start further exploration from.
  * @return {number[][]}
  */
-
+/** if you want to test the function combinationSumRecursive 
+ * I suggest to copy code from line 10 to line 40 and able debugger in line 28
+ */
 const combinationSumRecursive = (
+    candidates,
+    remainingSum,
+    finalCombinations =[],
+    currentCombination =[],
+    startFrom = 0,
+    )=>{
+        if (remainingSum ===0){ 
+          finalCombinations.push(currentCombination.slice());
+        }
+        
+        if(remainingSum < 0) {
+          return finalCombinations;
+        }
+        
+        for (let i= startFrom; i < candidates.length; i++){
+          //debugger
+          const current = candidates[i];
+          currentCombination.push(current);
+          
+          //add the next position of the array candidates and try again until remainingSum < 0
+          combinationSumRecursive(candidates, remainingSum - current, finalCombinations,currentCombination, i);
+
+          //delete candidate
+          currentCombination.pop();
+
+          
+
+        }
+       return finalCombinations
     
-  }
+      }    
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.

3075abbe5f6cbd4a4412d17ec334f10c47d11c18
Gerardo Garavito <garavitogerardo@gmail.com>
2020-08-19 21:39:20 -0500

    challenge 06 done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 /**
- * @param {number[]} candidates - candidate numbers we're picking from.
- * @param {number} remainingSum - remaining sum after adding candidates to currentCombination.
+ * @param {number[]} candidates - candidate numbers we're picking from. w[]
+ * @param {number} remainingSum - remaining sum after adding candidates to currentCombination. r
  * @param {number[][]} finalCombinations - resulting list of combinations.
- * @param {number[]} currentCombination - currently explored candidates.
- * @param {number} startFrom - index of the candidate to start further exploration from.
+ * @param {number[]} currentCombination - currently explored candidates. s[elements]; y reduce() para valor
+ * @param {number} startFrom - index of the candidate to start further exploration from. k
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, finalCombinations=[], currentCombination=[], startFrom = 0) => {
+
+  if (remainingSum === 0) {
+    finalCombinations.push(currentCombination)
+    currentCombination = []
+    return finalCombinations
+  } else if (remainingSum < 0){
+    return []
+  }else {
+    let i = startFrom
+    while (i < candidates.length) {
+      let number = candidates[i]
+      combinationSumRecursive(candidates, remainingSum - number, finalCombinations, [...currentCombination, number], i)
+      i++
+    }
+    return finalCombinations
   }
+}
   
   /**
    * Backtracking algorithm of finding all possible combination for specific sum.
